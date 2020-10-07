1503a138a3443d672eddefa1c0f0ca96a044d268
dmfananddu <af0706@yandex.ru>
2020-10-07 14:01:30 +0300

    fifth printing

--- main.c
+++ main.c
@@ -18,5 +18,6 @@ int main(int argc, char **argv)
  printf("Hello %s %s!\n", first, last);
  printf("Hello %s %s!\n", first, last);
  printf("Hello %s %s!\n", first, last);
+ printf("Hello %s %s!\n", first, last);
  return 0;
 }

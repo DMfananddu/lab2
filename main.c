99752b10c48006f86dd9a21682808c0a6d642213
dmfananddu <af0706@yandex.ru>
2020-10-07 14:22:12 +0300

    Revert "fifth printing"
    This reverts commit 1503a138a3443d672eddefa1c0f0ca96a044d268.

--- main.c
+++ main.c
@@ -18,6 +18,5 @@ int main(int argc, char **argv)
  printf("Hello %s %s!\n", first, last);
  printf("Hello %s %s!\n", first, last);
  printf("Hello %s %s!\n", first, last);
- printf("Hello %s %s!\n", first, last);
  return 0;
 }
